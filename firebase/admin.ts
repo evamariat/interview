780b780faae2d805a4032fda92721b30df25302d
“evamariat” <eva.maria.truve@iCloud.com>
2026-03-07 13:42:05 +0200

    png and svg files

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -4,12 +4,22 @@ import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
   const apps = getApps();
-  
+
+if (!process.env.FIREBASE_PROJECT_ID) {
+  throw new Error("FIREBASE_PROJECT_ID is not set");
+}
+if (!process.env.FIREBASE_CLIENT_EMAIL) {
+  throw new Error("FIREBASE_CLIENT_EMAIL is not set");
+}
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error("FIREBASE_PRIVATE_KEY is not set");
+}
+
   if (apps.length === 0) {
     initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID!,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,  // Fixed typo
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       }),
     });
